899f747416f2006178826ea01d95d43b25729a75
chintufireman <harshwardhanshetty6@gmail.com>
2026-03-23 17:08:01 +0530

    cors

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from fastapi import FastAPI
 from routes.interview import router
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
-
-app.include_router(router)
-
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.include_router(router)
+
+
+
 
 @app.get("/")
 def home():
